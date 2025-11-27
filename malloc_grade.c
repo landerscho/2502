dc114b1058723943942cf15e7e2d5802a39e426c
landerscho <2kindawin@gmail.com>
2025-11-27 11:45:53 +0900

    Create malloc_grade.c

---
+++ malloc_grade.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int *list;
+  int i, students, sum=0;
+
+  printf("학생의 수:");
+  scanf("%d",&students);
+  list=(int*)malloc(students*sizeof(int));
+  if (list == NULL){
+    printf("동적 메모리 할당 오류\n");
+    exit(1);
+  }
+  for (i=0;i<students;i++){
+    printf("학생 #%d 성적:", i+1);
+    scanf("%d", &list[i]);
+  }
+  for (i=0; i<students;i++)
+    sum +=list[i];
+  printf("성적 평균=%.2f\n", (double)sum/students);
+  free(list);
+  return 0;
+}
